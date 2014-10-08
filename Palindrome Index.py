aff51bac37a114f8e287b62454d519bb97789fd7
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2014-10-08 18:02:32 +0800

    String

---
+++ Palindrome Index.py
@@ -0,0 +1,70 @@
+"""
+You are given a string of lowercase letters. Your task is to figure out the index of the character on whose removal will
+make the string a palindrome. There will always be a valid solution.
+
+In case string is already palindrome, then -1 is also a valid answer along with possible indices.
+
+Input Format
+The first line contains T i.e. number of test cases.
+T lines follow, each line containing a string.
+"""
+__author__ = 'Danyang'
+class Solution_TLE(object):
+    def solve(self, cipher):
+        """
+        Algorithm:
+        brutal force. O(N^2)
+        Non-brainer
+        :param cipher: the cipher
+        """
+        for i in xrange(len(cipher)):
+            if self.__is_palindrome(cipher[:i]+cipher[i+1:]):
+                return i
+
+        return -1
+
+
+    def __is_palindrome(self, s):
+        return s==s[::-1]
+
+
+class Solution(object):
+    def solve(self, cipher):
+        """
+        Algorithm:
+        Guarantee one and only one answer
+        O(N) 
+        :param cipher: the cipher
+        """
+        l = len(cipher)
+        start = 0
+        end = l-1
+        while start<end and cipher[start]==cipher[end]:
+            start += 1
+            end -= 1
+
+        if self.__is_palindrome(cipher[:start]+cipher[start+1:]):
+            return start
+        if self.__is_palindrome(cipher[:end]+cipher[end+1:]):
+            return end
+
+        if start>=end:
+            return -1
+
+    def __is_palindrome(self, s):
+        return s==s[::-1]
+
+
+if __name__=="__main__":
+    import sys
+    f = open("1.in", "r")
+    # f = sys.stdin
+    testcases = int(f.readline().strip())
+
+    for t in xrange(testcases):
+        # construct cipher
+        cipher = f.readline().strip()
+
+        # solve
+        s = "%s\n"%(Solution().solve(cipher))
+        print s,
---
+++ Pangrams.py
@@ -0,0 +1,50 @@
+"""
+Roy wanted to increase his typing speed for programming contests. So, his friend advised him to type the sentence "The
+quick brown fox jumps over the lazy dog" repeatedly because it is a pangram. ( pangrams are sentences constructed by
+using every letter of the alphabet at least once. )
+
+After typing the sentence several times, Roy became bored with it. So he started to look for other pangrams.
+
+Given a sentence s, tell Roy if it is a pangram or not.
+
+Input Format
+
+Input consists of a line containing s.
+"""
+__author__ = 'Danyang'
+class Solution(object):
+    def solve(self, cipher):
+        """
+        Algorithm:
+        bucket, O(N). Non-brainer
+        :param cipher: the cipher
+        """
+        bucket = [False for _ in xrange(26)]
+        for char in cipher:
+            char = char.lower()
+            ind = ord(char)-ord('a')
+            try:
+                bucket[ind] = True
+            except IndexError:
+                pass
+
+        is_pangram = all(bucket)
+        if is_pangram:
+            return "pangram"
+        else:
+            return "not pangram"
+
+
+if __name__=="__main__":
+    import sys
+    f = open("1.in", "r")
+    # f = sys.stdin
+    testcases = 1
+
+    for t in xrange(testcases):
+        # construct cipher
+        cipher = f.readline().strip()
+
+        # solve
+        s = "%s\n"%(Solution().solve(cipher))
+        print s,
