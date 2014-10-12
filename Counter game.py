358dd1cb2b699b0f042cf27c1bf62bad36d504b6
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2014-10-12 15:33:19 +0800

    bit manipulation

---
+++ Counter game.py
@@ -0,0 +1,57 @@
+"""
+Louise and Richard play a game. They have a counter set to N. Louise gets the first turn and the turns alternate
+thereafter. In the game, they perform the following operations.
+
+If N is not a power of 2, they reduce the counter by the largest power of 2 less than N.
+If N is a power of 2, they reduce the counter by half of N.
+The resultant value is the new N which is again used for subsequent operations.
+The game ends when the counter reduces to 1, i.e., N == 1, and the last person to make a valid move wins.
+
+Given N, your task is to find the winner of the game.
+
+Update If they set counter to 1, Richard wins, because its Louise' turn and she cannot make a move.
+
+Input Format
+The first line contains an integer T, the number of testcases.
+T lines follow. Each line contains N, the initial number set in the counter.
+"""
+__author__ = 'Danyang'
+class Solution(object):
+    def solve(self, cipher):
+        """
+        main solution function
+        :param cipher: the cipher
+        """
+        N = cipher
+        turn = 0
+        while N>1:
+            turn += 1
+            if N&(N-1)==0:
+                N /= 2
+            else:
+                num = 1
+                while num<N:
+                    num <<=1
+                num >>= 1
+                N -= num
+
+        if turn&1==0:
+            return "Richard"
+        else:
+            return "Louise"
+
+
+
+if __name__=="__main__":
+    import sys
+    f = open("1.in", "r")
+    # f = sys.stdin
+    testcases = int(f.readline().strip())
+
+    for t in xrange(testcases):
+        # construct cipher
+        cipher = int(f.readline().strip())
+
+        # solve
+        s = "%s\n"%(Solution().solve(cipher))
+        print s,
---
+++ Xoring Ninja.py
@@ -0,0 +1,46 @@
+"""
+Given a list containing N integers, calculate the XOR_SUM of all the non-empty subsets of the list and print the value
+of sum % (109 + 7).
+
+XOR operation on a list (or a subset of the list) is defined as the XOR of all the elements present in it.
+E.g. XOR of list containing elements {A,B,C} = ((A^B)^C), where ^ represents XOR.
+
+E.g. XOR_SUM of list A having three elements {X1, X2, X3} can be given as follows.
+All non-empty subsets will be {X1, X2, X3, (X1,X2), (X2,X3), (X1,X3), (X1,X2,X3)}
+
+XOR_SUM(A) = X1 + X2 + X3 + X1^X2 + X2^X3 + X1^X3 + ((X1^X2)^X3)
+
+Input Format
+An integer T, denoting the number of testcases. 2T lines follow.
+Each testcase contains two lines, first line will contains an integer N followed by second line containing N integers
+separated by a single space.
+"""
+__author__ = 'Danyang'
+class Solution(object):
+    def solve(self, cipher):
+        """
+        Algorithm:
+        From the prospective of bit
+        http://math.stackexchange.com/questions/712487/finding-xor-of-all-subsets
+        :param cipher: the cipher
+        """
+        length, lst = cipher
+
+        return reduce(lambda x, y: x|y, lst)*2**(length-1)%(10**9+7)
+
+
+
+if __name__=="__main__":
+    import sys
+    f = open("1.in", "r")
+    # f = sys.stdin
+    testcases = int(f.readline().strip())
+
+    for t in xrange(testcases):
+        # construct cipher
+        length = int(f.readline().strip())
+        lst = map(lambda x: int(x), f.readline().strip().split(" "))
+        cipher = [length, lst]
+        # solve
+        s = "%s\n"%(Solution().solve(cipher))
+        print s,
