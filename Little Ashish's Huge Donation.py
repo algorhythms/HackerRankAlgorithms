8d95ba27c1904c6e515a1bf664ed9055346a4b07
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2014-11-27 10:48:27 +0800

    number theory

---
+++ Little Ashish's Huge Donation.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Problem Statement
+
+Little Ashish is doing internship at multiple places. Instead of giving parties to his friends he decided to donate
+candies to children. He likes solving puzzles and playing games. Hence he plays a small game. Suppose there are N
+children. The rules of the game are:
+
+The ith child gets i2 candies (1≤i≤N).
+
+The yth child cannot get a candy until and unless all the children before him (1≤i<y) gets candies according to rule
+number 1.
+
+One of his jealous friends, Pipi, asks him "Given X (the number of candies) how many children will you be able to
+serve?". Little Ashish fears calculations and cannot solve this problem so he leaves this problem to the worthy
+programmers of the world. Help little Ashish in finding the solution.
+
+Input Format
+The first line contains T i.e. number of test cases.
+T lines follow, each line containing an integer X.
+"""
+__author__ = 'Danyang'
+class Solution(object):
+    def solve(self, cipher):
+        """
+        Bin search + sum of squares
+        :param cipher: the cipher
+        """
+        l = 1
+        h = cipher
+        while l<=h:
+            mid = (l+h)/2
+            if self.sum_of_squares(mid)<=cipher:
+                l = mid+1
+            else:
+                h = mid-1
+
+        l -= 1
+        return l
+
+    def sum_of_squares(self, n):
+        return n*(n+1)*(2*n+1)/6
+
+
+if __name__=="__main__":
+    import sys
+    f = open("1.in", "r")
+    # f = sys.stdin
+    testcases = int(f.readline().strip())
+
+    for t in xrange(testcases):
+        # construct cipher
+        cipher = int(f.readline().strip())
+
+        # solve
+        s = "%s\n"%(Solution().solve(cipher))
+        print s,
---
+++ Reverse Game.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Problem Statement
+
+Akash and Akhil are playing a game. They have N balls numbered from 0 to N−1. Akhil asks Akash to reverse the position
+of the balls, i.e., to change the order from say, 0,1,2,3 to 3,2,1,0. He further asks Akash to reverse the position of
+the balls N times, each time starting from one position further to the right, till he reaches the last ball. So, Akash
+has to reverse the positions of the ball starting from 0th position, then from 1st position, then from 2nd position and
+so on. At the end of the game, Akhil will ask Akash the final position of any ball numbered K. Akash will win the game,
+if he can answer. Help Akash.
+
+Input Format
+The first line contains an integer T, i.e., the number of the test cases.
+The next T lines will contain two integers N and K.
+
+Output Format
+Print the final index in array.
+"""
+__author__ = 'Danyang'
+class Solution(object):
+    def solve(self, cipher):
+        """
+        Math
+        group into pairs
+        :param cipher: the cipher
+        """
+        N, K = cipher
+        if K<N/2:
+            return 2*K+1
+        else:
+            return 2*(N-1-K)
+
+
+if __name__=="__main__":
+    import sys
+    f = open("1.in", "r")
+    # f = sys.stdin
+    testcases = int(f.readline().strip())
+
+    for t in xrange(testcases):
+        # construct cipher
+        cipher = map(int, f.readline().strip().split(' '))
+
+        # solve
+        s = "%s\n"%(Solution().solve(cipher))
+        print s,
