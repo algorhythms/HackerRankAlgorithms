8cbd44a120cbb6478105475bf6148a21238be199
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-04-02 10:46:42 +0800

    basic

---
+++ Funny String.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Problem Statement
+
+Suppose you have a string S which has length N and is indexed from 0 to N−1. String R is the reverse of the string S.
+The string S is funny if the condition |Si−Si−1|=|Ri−Ri−1| is true for every i from 1 to N−1.
+
+(Note: Given a string str, stri denotes the ascii value of the ith character (0-indexed) of str. |x| denotes the
+absolute value of an integer x)
+"""
+__author__ = 'Danyang'
+
+
+class Solution(object):
+    def solve(self, cipher):
+        """
+        main solution function
+        :param cipher: the cipher
+        """
+        s = cipher
+        r = s[::-1]
+        s = map(ord, list(s))
+        r = map(ord, list(r))
+        for i in xrange(1, len(s)):
+            if abs(s[i]-s[i-1])!=abs(r[i]-r[i-1]):
+                return "Not Funny"
+        return "Funny"
+
+
+if __name__=="__main__":
+    import sys
+    f = open("0.in", "r")
+    # f = sys.stdin
+    solution = Solution()
+    testcases = int(f.readline().strip())
+
+    for t in xrange(testcases):
+        # construct cipher
+        cipher = f.readline().strip()
+
+        # solve
+        s = "%s\n"%(solution.solve(cipher))
+        print s,
---
+++ Strange Grid.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+A strange grid has been recovered from an old book. It has 5 columns and infinite number of rows. The bottom row is
+considered as the first row. First few rows of the grid are like this:
+
+..............
+
+..............
+
+20 22 24 26 28
+
+11 13 15 17 19
+
+10 12 14 16 18
+
+ 1  3  5  7  9
+
+ 0  2  4  6  8
+The grid grows upwards forever!
+
+Your task is to find the integer in cth column in rth row of the grid.
+"""
+__author__ = 'Danyang'
+
+
+class Solution(object):
+    def solve(self, cipher):
+        """
+        main solution function
+        :param cipher: the cipher
+        """
+        r, c = cipher
+        r, c = r-1, c-1
+
+        return r/2*10+r%2+c*2
+
+
+if __name__=="__main__":
+    import sys
+    f = open("0.in", "r")
+    # f = sys.stdin
+    solution = Solution()
+
+    cipher = map(int, f.readline().strip().split(' '))
+
+    # solve
+    s = "%s\n"%(solution.solve(cipher))
+    print s,
